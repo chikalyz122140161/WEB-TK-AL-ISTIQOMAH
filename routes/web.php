f3233bef979e1c46699c2f6987436ff0c089bce8
chikalyzkayla <chikalyzkaylap@gmail.com>
2026-03-02 23:31:33 +0700

    DATABASE

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\GuruController;
+use App\Http\Controllers\OrangTuaController;
+
+// Dashboard Admin
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+// Dashboard Guru
+Route::get('/guru/dashboard', [GuruController::class, 'dashboard'])->name('guru.dashboard');
+
+// Dashboard Orang Tua
+Route::get('/orangtua/dashboard', [OrangTuaController::class, 'dashboard'])->name('orangtua.dashboard');
